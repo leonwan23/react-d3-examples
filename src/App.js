fb9543857b20545dd7d6b673bea444b6b439ff69
leonwan23 <leon_wan23@hotmail.com>
2019-12-28 23:52:00 +0800

    refactored to expense app, adding category feature donee

--- src/App.js
+++ src/App.js
@@ -1,19 +1,113 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import arcData from "./data/arc_data.json";
 
-import Arc from "./visualizations/Arc";
+import Category from "./visualizations/Category";
 
-function App() {
+const width = 1280;
+const height = 1800;
 
-  
+const ENTER_CODE = 13;
 
-  return (
-    <div className="App">
-      <Arc data={arcData} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        {
+          id: 1,
+          name: "Parking"
+        },
+        {
+          id: 2,
+          name: "Shopping"
+        }
+      ],
+      categoryBeingAdded: null,
+      inputPosition: { x: 0, y: 0 }
+    };
+  }
+
+  componentDidMount() {
+    this.updateInputPosition();
+
+    window.addEventListener("resize", this.updateInputPosition);
+  }
+
+  updateInputPosition = () => {
+    const { x, y } = document
+      .getElementById("category-input")
+      .getBoundingClientRect();
+    this.setState({ inputPosition: { x, y } });
+  };
+
+  startCategory = event => {
+    const categoryBeingAdded = {
+      id: Math.random() * 1000,
+      name: event.target.value
+    };
+    this.setState({ categoryBeingAdded });
+  };
+
+  clearCategory = event => {
+    event.target.value = "";
+    event.target.blur();
+    this.setState({ categoryBeingAdded: null });
+  };
+
+  addCategory = event => {
+    if (event.keyCode === ENTER_CODE) {
+      // take the value of the input and create new category
+      var category = Object.assign(this.state.categoryBeingAdded, {
+        name: event.target.value,
+        fx: null,
+        fy: null
+      });
+
+      // clear out the input form on successful submit
+      event.target.value = "";
+      event.target.blur();
+
+      this.setState({
+        categories: [...this.state.categories, category],
+        categoryBeingAdded: null
+      });
+    }
+  };
+
+  deleteCategory = (category) => {
+
+  }
+
+  render() {
+    const { categories, categoryBeingAdded, inputPosition } = this.state;
+
+    return (
+      <div className="App">
+        <input
+          id="category-input"
+          className="category-input"
+          placeholder="Add category"
+          onFocus={this.startCategory}
+          onBlur={this.clearCategory}
+          onKeyDown={this.addCategory}
+        ></input>
+        <svg
+          className="svg-container"
+          width={width}
+          height={height}
+          viewBox={`0 0 ${width} ${height}`}
+        >
+          <Category
+            data={categories}
+            width={width}
+            height={height}
+            categoryBeingAdded={categoryBeingAdded}
+            inputPosition={inputPosition}
+          />
+        </svg>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/visualizations/Category.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+import * as d3 from "d3";
+import chroma from "chroma-js";
+
+const grey = "rgba(232, 236, 241, 1)";
+
+const colorScale = chroma.scale(["#53c3ac", "#f7e883", "#e85178"]);
+const radius = 50;
+
+const simulation = d3
+  .forceSimulation()
+  .alphaDecay(0.001)
+  .velocityDecay(0.3)
+  .force(
+    "collide",
+    d3.forceCollide(d => d.radius + 10)
+  )
+  .force(
+    "x",
+    d3.forceX(d => d.focusX)
+  )
+  .force(
+    "y",
+    d3.forceY(d => d.focusY)
+  )
+  .stop();
+const drag = d3.drag();
+
+export default class Bubble extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    simulation.on("tick", this.forceTick);
+    drag
+      .on("start", this.dragStart)
+      .on("drag", this.onDrag)
+      .on("end", this.dragEnd);
+  }
+
+  componentDidMount() {
+    const { width, height } = this.props;
+    this.container = d3.select("#categories");
+
+    this.calculateData();
+    this.drawCircles();
+
+    simulation
+      .nodes(this.data)
+      .alpha(0.9)
+      .restart();
+  }
+
+  componentDidUpdate() {
+    this.calculateData();
+    this.drawCircles();
+
+    simulation
+      .nodes(this.data)
+      .alpha(0.9)
+      .restart();
+  }
+
+  calculateData = () => {
+    const {
+      data,
+      categoryBeingAdded,
+      width,
+      height,
+      inputPosition
+    } = this.props;
+    this.data = data.slice();
+    this.data.map(d => {
+      return Object.assign(d, {
+        focusX: width / 2,
+        focusY: height / 6,
+        radius
+      });
+    });
+
+    if (categoryBeingAdded) {
+      this.data.push(
+        Object.assign(categoryBeingAdded, {
+          fx: inputPosition.x - 20,
+          fy: -5,
+          radius
+        })
+      );
+    }
+  };
+
+  drawCircles = () => {
+    this.circles = this.container.selectAll("g").data(this.data);
+
+    //exit + remove
+    this.circles.exit().remove();
+
+    //enter
+    const enter = this.circles.enter().append("g");
+    enter
+      .append("circle")
+      .attr("r", radius)
+      .attr("fill", grey)
+      .attr("stroke-width", 1)
+      .attr("stroke", chroma(grey).darken())
+      .call(drag);
+    enter
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("dy", ".35em")
+      .attr("font-size", 14)
+      .style("pointer-events", "none");
+
+    //enter + update
+    this.circles = enter.merge(this.circles);
+    this.circles.select("text").text(d => d.name);
+  };
+
+  forceTick = () => {
+    this.circles.attr("transform", d => "translate(" + [d.x, d.y] + ")");
+  };
+
+  dragStart = () => {
+    simulation.alphaTarget(0.3).restart();
+    d3.event.subject.fx = d3.event.subject.x;
+    d3.event.subject.fy = d3.event.subject.y;
+  };
+
+  onDrag = () => {
+    d3.event.subject.fx = d3.event.x;
+    d3.event.subject.fy = d3.event.y;
+  };
+
+  dragEnd = () => {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d3.event.subject.fx = null;
+    d3.event.subject.fy = null;
+  };
+
+  render() {
+    return <g id="categories"></g>;
+  }
+}
