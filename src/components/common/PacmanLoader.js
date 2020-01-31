3067957fb1d26b7e71a3643494efa10025ecfbc9
Wan <lewan@deloitte.com>
2020-01-31 23:29:09 +0800

    modified loader

---
+++ src/components/common/PacmanLoader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./common.scss";
+
+export default function Loader() {
+  return (
+    <div className="la-pacman">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
